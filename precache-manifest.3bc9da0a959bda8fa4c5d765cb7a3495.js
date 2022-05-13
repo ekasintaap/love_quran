df3f360a1c99063e1ea0b01dda07dcd802e1306b
ekasintaap <72989524+ekasintaap@users.noreply.github.com>
2022-05-13 21:33:30 +0700

    Updates

---
+++ precache-manifest.3bc9da0a959bda8fa4c5d765cb7a3495.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "dee7fd338c1847d81b8a",
+    "url": "/love_quran/static/css/main.939825e3.chunk.css"
+  },
+  {
+    "revision": "dee7fd338c1847d81b8a",
+    "url": "/love_quran/static/js/main.dee7fd33.chunk.js"
+  },
+  {
+    "revision": "a5456eae47356bfb2ce3",
+    "url": "/love_quran/static/js/1.a5456eae.chunk.js"
+  },
+  {
+    "revision": "a2c2167b110670d5cde9",
+    "url": "/love_quran/static/css/2.0b9f69c4.chunk.css"
+  },
+  {
+    "revision": "a2c2167b110670d5cde9",
+    "url": "/love_quran/static/js/2.a2c2167b.chunk.js"
+  },
+  {
+    "revision": "8617c92dc5fa4de5bd90",
+    "url": "/love_quran/static/js/runtime~main.8617c92d.js"
+  },
+  {
+    "revision": "2e44b9a1a7e70d9cdf307f39340a48d5",
+    "url": "/love_quran/static/media/surahbg.2e44b9a1.png"
+  },
+  {
+    "revision": "15e0bc9ad650cc976c592866803b6d67",
+    "url": "/love_quran/static/media/profile.15e0bc9a.png"
+  },
+  {
+    "revision": "54871c9ca1e17fd605b3b6fa59d69cc4",
+    "url": "/love_quran/static/media/backgroundAtas.54871c9c.png"
+  },
+  {
+    "revision": "daed3a82f594a8a4ce2a92633ad5019c",
+    "url": "/love_quran/index.html"
+  }
+];
